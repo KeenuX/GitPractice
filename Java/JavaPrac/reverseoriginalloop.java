6836692b0914dc8962108b3957817cced6eba897
KeenuX <keenumeehu2@gmail.com>
2026-07-25 14:09:20 +0530

    Auto-save: 2026-07-25 14:09:20

--- Java/JavaPrac/reverseoriginalloop.java
+++ Java/JavaPrac/reverseoriginalloop.java
@@ -4,7 +4,7 @@ public class reverseoriginalloop{
         for (int i = 0; i < array.length/2; i++) {
             int temp=array[i];
             array[i]=array[array.length-i-1];
-            array[]
+            array[array.length]
         }
     }
 }

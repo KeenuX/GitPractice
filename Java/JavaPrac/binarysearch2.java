59fb62ac1980baab70fa35efd64fa5ccb3be8e0f
KeenuX <keenumeehu2@gmail.com>
2026-08-04 14:27:59 +0530

    Q34-Leet

--- Java/JavaPrac/binarysearch2.java
+++ Java/JavaPrac/binarysearch2.java
@@ -1,6 +1,40 @@
-public class binarysearch2 {
+class Solution {   
 
-    public static void main(String[] args) {
+    private int binarysearch(int[] nums, int target){
+        int low=0;
+        int high=nums.length-1;
+        while(low<=high){
+            int middle=(high+low)/2;
+            if(nums[middle]==target)
+                return middle;
+
+            else if(nums[middle]>target)
+                high=middle-1;
+            else
+                low=middle+1;
+         }
+         return -1;
+    }
+
+    public int[] searchRange(int[] nums, int target) {
+        int[] ans=new int[2];
+        int index= binarysearch(nums, target);
+        if(index==-1){
+            ans[0]=-1;
+            ans[1]=-1;
+            return ans;
+        }
+        int low=index;
+        int high=index;
+        while(low>=0 && nums[low]==target)
+            low--;
+        while(high<nums.length && nums[high]==target)
+            high++;
+
+        ans[0]= low+1;
+        ans[1]=high-1;
+        return ans;
         
     }
+
 }
